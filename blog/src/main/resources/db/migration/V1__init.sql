0051465865ceadb53b0f0558b859609ae313708b
Harit Yadav <yadav.harit@gmail.com>
2022-02-19 20:08:45 +0530

    Flyway scripts for creating table and uploading init data

--- blog/src/main/resources/db/migration/V1__init.sql
+++ blog/src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,37 @@
-CREATE TABLE users (
+CREATE TABLE post (
   id bigint(20) NOT NULL AUTO_INCREMENT,
-  username varchar(100) NOT NULL,
-  first_name varchar(50) NOT NULL,
-  last_name varchar(50) DEFAULT NULL,
+  title varchar(100) NOT NULL
+  	CONSTRAINT title_not_empty CHECK(
+            LENGTH(title) > 0 
+        ),
+  content LONGTEXT NOT NULL
+  	CONSTRAINT content_not_empty CHECK(
+            LENGTH(content) > 0 
+        ),
+  createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  username varchar(50) NOT NULL
+  	CONSTRAINT username_not_empty CHECK(
+            LENGTH(username) > 0 
+        ),
+  PRIMARY KEY (id),
+  UNIQUE KEY UK_posttitle (title)
+);
+
+CREATE TABLE user (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  username varchar(50) NOT NULL
+  	CONSTRAINT username_not_empty CHECK(
+            LENGTH(username) > 0 
+        ),
+  password TINYTEXT NOT NULL
+  	CONSTRAINT password_not_empty CHECK(
+            LENGTH(password) > 0 
+        ),
+  email varchar(50) NOT NULL
+  	CONSTRAINT email_not_empty CHECK(
+            LENGTH(email) > 0 
+        ),
   PRIMARY KEY (id),
   UNIQUE KEY UK_username (username)
 );
---
+++ blog/src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+  post (id, title, content, username)
+VALUES(1, 'nd', 'kkjsf', 'dummy');
+
+INSERT into user values(1, 'dummy', 'dummy', 'dummy@dummy.com');
