3ce99912c8d067772dd128a06b25787c579b37eb
Harit Yadav <yadav.harit@gmail.com>
2022-02-22 20:03:59 +0530

    New User Registration request breaks db constraints

---
+++ blog/src/main/java/com/blog/demo/exception/BadNewUserRequestException.java
@@ -0,0 +1,10 @@
+package com.blog.demo.exception;
+
+public class BadNewUserRequestException extends Exception {
+	
+	public BadNewUserRequestException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
