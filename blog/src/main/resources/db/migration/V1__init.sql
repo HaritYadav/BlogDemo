51ac3b361aac3eb11cb2851f367e3b303a3376af
Harit Yadav <yadav.harit@gmail.com>
2022-02-24 21:42:31 +0530

    Edited scripts to work with older version MySQL

--- blog/src/main/resources/db/migration/V1__init.sql
+++ blog/src/main/resources/db/migration/V1__init.sql
@@ -1,19 +1,22 @@
 CREATE TABLE post (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   title varchar(100) NOT NULL
-  	CONSTRAINT title_not_empty CHECK(
-            LENGTH(title) > 0 
-        ),
+--  	CONSTRAINT title_not_empty CHECK(
+--            LENGTH(title) > 0 
+--        )
+    ,
   content LONGTEXT NOT NULL
-  	CONSTRAINT content_not_empty CHECK(
-            LENGTH(content) > 0 
-        ),
+--  	CONSTRAINT content_not_empty CHECK(
+--            LENGTH(content) > 0 
+--        )
+	,
   created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  updated_on TIMESTAMP, -- DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   username varchar(50) NOT NULL
-  	CONSTRAINT username_not_empty CHECK(
-            LENGTH(username) > 0 
-        ),
+--  	CONSTRAINT username_not_empty CHECK(
+--            LENGTH(username) > 0 
+--        )
+	,
   PRIMARY KEY (id),
   UNIQUE KEY UK_posttitle (title)
 );
@@ -21,17 +24,20 @@ CREATE TABLE post (
 CREATE TABLE user (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   username varchar(50) NOT NULL
-  	CONSTRAINT username_not_empty CHECK(
-            LENGTH(username) > 0 
-        ),
+--  	CONSTRAINT username_not_empty CHECK(
+--            LENGTH(username) > 0 
+--        )
+    ,
   password TINYTEXT NOT NULL
-  	CONSTRAINT password_not_empty CHECK(
-            LENGTH(password) > 0 
-        ),
+--  	CONSTRAINT password_not_empty CHECK(
+--            LENGTH(password) > 0 
+--        )
+    ,
   email varchar(50) NOT NULL
-  	CONSTRAINT email_not_empty CHECK(
-            LENGTH(email) > 0 
-        ),
+--  	CONSTRAINT email_not_empty CHECK(
+--            LENGTH(email) > 0 
+--        )
+    ,
   PRIMARY KEY (id),
   UNIQUE KEY UK_username (username)
 );
--- blog/src/main/resources/db/migration/V2__data.sql
+++ blog/src/main/resources/db/migration/V2__data.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-  post (id, title, content, username)
-VALUES(1, 'nd', 'kkjsf', 'dummy');
+  post (id, title, content, username, updated_on)
+VALUES(1, 'nd', 'kkjsf', 'dummy', now());
 
 INSERT into user values(1, 'dummy', '$2a$10$aXVbFGFO0vpUyfT6cKEvnO.h5YUmwcqexQFrH4IKEBvmcnO3d/IjW', 'dummy@dummy.com');
