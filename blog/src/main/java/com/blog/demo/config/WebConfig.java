4d37df0dcaf3e3e686f6b424f75de2143ff9bf0b
Harit Yadav <yadav.harit@gmail.com>
2022-03-03 22:57:10 +0530

    Enabled CORS support

---
+++ blog/src/main/java/com/blog/demo/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.blog.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+			.allowedOrigins("http://localhost:4200", "http://localhost:8080")
+			.allowedMethods("GET", "POST")
+			.maxAge(3600L)
+			.allowedHeaders("*")
+			.exposedHeaders("Authorization")
+			.allowCredentials(true);
+	}
+
+}
